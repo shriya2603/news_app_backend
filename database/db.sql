6574843c019c9dbb1edcc8416132aa0393f63ab5
shriya2603 <sundriyalshriya@gmail.com>
2022-02-17 09:39:34 +0530

    Updated queries and inserted sample data in tables

--- database/db.sql
+++ database/db.sql
@@ -1,26 +1,39 @@
 -- news feeds 
-USE DATABASE newsapp;
+CREATE DATABASE newsapp;
 
+-- create Feeds table 
 CREATE TABLE feeds (
-    id PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     article_thumbnail BYTEA,
     headline VARCHAR(200) NOT NULL,
     category VARCHAR(200) NOT NULL,
     author_name VARCHAR(200) NOT NULL,
-    upload_time TIMESTAMP WITHOUT TIMEZONE DEFAULT (NOW() AT TIMEZONE 'UTC')
+    upload_time TIMESTAMP DEFAULT NOW()
 );
 
+-- Create Author table 
 CREATE TABLE author (
-    author_id PRIMARY KEY,
+    author_id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     email VARCHAR(200) NOT NULL,
     password VARCHAR(12) NOT NULL,
     phoneNo NUMERIC(10) NOT NULL,
     date_of_birth DATE NOT NULL,
     time_of_birth TIME NOT NULL,
-    gender NOT NULL,
-    marital_status NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    marital_status VARCHAR(20) NOT NULL,
     language VARCHAR(20) NOT NULL,
-    profile_picture BYTEA NOT NULL,
-    CHECK(gender IN ("female", "male"))
+    profile_picture BYTEA NOT NULL
 );
+
+-- Sample feeds data 
+INSERT INTO feeds (article_thumbnail,headline, category, author_name) VALUES (pg_read_binary_file('C:\studies\study_material\Workspace\Projects\news_api\database\snakeNews.png'),'Meet the man who rescues', 'general','Ram Sharma'); 
+
+INSERT INTO feeds (article_thumbnail,headline, category, author_name) VALUES (pg_read_binary_file('C:\studies\study_material\Workspace\Projects\news_api\database\technews.png'),'Russia-Linked Hackers Bagged $400 Million in Crypto From Ransomware Attacks, Reports Chainalysis', 'technology',' Shomik Sen Bhattacharjee');
+
+INSERT INTO feeds (article_thumbnail,headline, category, author_name) VALUES (pg_read_binary_file('C:\studies\study_material\Workspace\Projects\news_api\database\technews.png'),'Kremlin Denies Russia Behind Ukraine Cyberattack', 'technology','Agence France-Presse');
+
+INSERT INTO feeds (article_thumbnail,headline, category, author_name) VALUES (pg_read_binary_file('C:\studies\study_material\Workspace\Projects\news_api\database\technews.png'),'Apple Spotted Registering Three New Mac Models on EEC Ahead of Rumoured March 8 Event', 'technology','Sourabh Kulesh ');
+
+-- sample user data 
+
--- routes.js
+++ routes.js
@@ -17,3 +17,24 @@ router.get("/getFeeds", async (request, response) => {
 });
 
 module.exports = router;
+
+// // Method to set salt and hash the password for a user
+// UserSchema.methods.setPassword = function(password) {
+
+//  // Creating a unique salt for a particular user
+//     this.salt = crypto.randomBytes(16).toString('hex');
+
+//     // Hashing user's salt and password with 1000 iterations,
+
+//     this.hash = crypto.pbkdf2Sync(password, this.salt,
+//     1000, 64, `sha512`).toString(`hex`);
+// };
+
+// // Method to check the entered password is correct or not
+// UserSchema.methods.validPassword = function(password) {
+//     var hash = crypto.pbkdf2Sync(password,
+//     this.salt, 1000, 64, `sha512`).toString(`hex`);
+//     return this.hash === hash;
+// };
+
+//https://medium.com/@alexishevia/storing-passwords-securely-with-postgresql-and-pgcrypto-87151a318998
